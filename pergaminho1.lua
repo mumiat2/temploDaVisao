632f14455127eaea612552a4130b138673a8394d
mumiat2 <125617620+mumiat2@users.noreply.github.com>
2023-02-20 16:58:52 -0300

    Update pergaminho1.lua

--- pergaminho1.lua
+++ pergaminho1.lua
@@ -2,14 +2,14 @@ local pergaminhoExoriu = Action()
 
 function pergaminhoExoriu.onUse(player, item, fromPosition, target, toPosition, isHotkey)
 	local magia = "Berserkiu"
-	local saber = player:getStorageValue(Storage.spell.attack.Berserkiu) == 1
+	local saber = player:getStorageValue(StdModule.learnSpell.Berserkiu) == 1
 				
 	if saber == 1 then
 	player:say("Você já aprendeu a magia deste scroll")
 
 	else
 	player:getPosition():sendMagicEffect(CONST_ME_SOUND_YELLOW)
-	player:getStorageValue(Storage.spell.attack.Berserkiu, 1)
+	player:getStorageValue(Storage.StdModule.learnSpell.Berserkiu, 1)
 	item:remove(1) -- vai consumir o scroll, remova isso se vc n quer que o scroll suma
 	player:say("Você aprendeu a magia "..magia..".")
 	end
